da8470809d5646bc6f5d92d93931e1936bc37ee6
Ram Naik <naikram1986@gmail.com>
2018-09-17 11:46:50 +0300

    made changes to my javascript

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 function button() {
-var side1 = document.getElementById("side1").value;
-var side2 = document.getElementById("side2").value;
-var side3 = document.getElementById("side3").value;
+var side1 = parseInt(document.getElementById("side1").value);
+var side3 = parseInt(document.getElementById("side3").value);
+var side2 = parseInt(document.getElementById("side2").value);
 var display = document.getElementById("answer");
 
 if ((side1===side2)&&(side1===side3)&&(side2===side3)){
   display.innerHTML ="This is an equilateral triangle!";
-} else if ((side1 + side2) <= side3 || (side1 + side3) <= side2|| (side2 + side3) <= side1){
-  display.innerHTML ="This is not a triangle!";
-} else if(side1===side2){
+  } else if (side1 === side2 || side1 === side3 || side2 === side3){
   display.innerHTML ="This is an isosceles triangle!";
-} else if(side1===side3){
-  display.innerHTML ="This is an isosceles triangle!";
-} else if(side2===side3){
-  display.innerHTML ="This is an isosceles triangle!";
-} else if((side1 !== side2) && (side1 !== side3) && (side2 !== side3)){
-  display.innerHTML ="This triangle is scalene!";
-} else {
+  } else if ((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1){
+  display.innerHTML = "This is not a triangle!";
+  } else if ( side1 !== side2 !== side3){
+  display.innerHTML = "This triangle is scalene!";
+  } else {
   display.innerHTML ="PLEASE ENTER NUMBERS ONLY!";
 }
 }
+
+
+
+// }else if ((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
+//   display.innerHTML = "This is not a triangle!";
+// }
