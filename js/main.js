8aaffac6d0165ca29efc04a727e3214f02614744
Ram Naik <naikram1986@gmail.com>
2018-09-17 09:34:40 +0300

    removed alert from js and made the alert in a new division

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
-var side1 = document.getElementById("side-1");
-var side1 = document.getElementById("side-2");
-var side1 = document.getElementById("side-3");
+function button() {
+var side1 = document.getElementById("side1").value;
+var side2 = document.getElementById("side2").value;
+var side3 = document.getElementById("side3").value;
+var display = document.getElementById("answer");
 
-if (side1===side2&&side1===side3&&side2===side3){
-  alert("This is an equilateral triangle!");
-} else if((side1+side2)<=side3){
-  alert("This is not a triangle!");
-} else if((side1 + side3)<=side2){
-  alert("This is not a triangle!");
-} else if((side1+side2)<=side3){
-  alert("This is not a triangle!");
+if ((side1===side2)&&(side1===side3)&&(side2===side3)){
+  display.innerHTML ="This is an equilateral triangle!";
+} else if ((side1 + side2) <= side3 || (side1 + side3) <= side2|| (side2 + side3) <= side1){
+  display.innerHTML ="This is not a triangle!";
 } else if(side1===side2){
-  alert("This is an isosceles triangle!");
+  display.innerHTML ="This is an isosceles triangle!";
 } else if(side1===side3){
-  alert("This is an isosceles triangle!");
+  display.innerHTML ="This is an isosceles triangle!";
 } else if(side2===side3){
-  alert("This is an isosceles triangle!");
-} else if(side1 !== side2 && side1 !== side3 && side2 !== side3){
-  alert("This triangle is scalene!");
+  display.innerHTML ="This is an isosceles triangle!";
+} else if((side1 !== side2) && (side1 !== side3) && (side2 !== side3)){
+  display.innerHTML ="This triangle is scalene!";
 } else {
-  alert("PLEASE ENTER NUMBERS ONLY!");
+  display.innerHTML ="PLEASE ENTER NUMBERS ONLY!";
 }
-
+}
