94f786cdd633faf99cb386233874c19fd6b895a2
Ram Naik <naikram1986@gmail.com>
2018-09-17 12:38:22 +0300

    made changes to js

--- js/main.js
+++ js/main.js
@@ -19,6 +19,3 @@ if ((side1===side2)&&(side1===side3)&&(side2===side3)){
 
 
 
-// }else if ((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
-//   display.innerHTML = "This is not a triangle!";
-// }
